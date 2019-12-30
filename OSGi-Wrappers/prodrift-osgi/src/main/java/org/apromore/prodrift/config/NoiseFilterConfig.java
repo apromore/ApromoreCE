5d7c1c2ec8fb28d7a9a6b9eb1d95a6cc24cb8423
Simon Raboczi <raboczi@icloud.com>
2019-12-30 11:50:20 +1000

    Added OSGi wrappers for libraries used by ProDrift and PQL.

---
+++ OSGi-Wrappers/prodrift-osgi/src/main/java/org/apromore/prodrift/config/NoiseFilterConfig.java
@@ -0,0 +1,17 @@
+package org.apromore.prodrift.config;
+
+public enum NoiseFilterConfig {
+
+	RemoveNoise,
+	RemoveNoise_sum,
+	RemoveNoise_max,
+	RemoveNoise_bottomRelations,
+	Aggregation,
+	RemoveInsignificantChanges,
+	NoFilter,
+	RemoveFrequencyDrift
+
+}
+
+
+
